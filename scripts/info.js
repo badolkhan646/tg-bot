2052eacf9469856cc8fc0c220a0f253993def657
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-11-29 15:51:20 +0600

    Create info.js

---
+++ scripts/info.js
@@ -0,0 +1,68 @@
+const moment = require('moment-timezone');
+
+module.exports = {
+    config: {
+        name: "info",
+        version: "1.0",
+        countDown: 20,
+        role: 0,
+        author: "MOHAMMAD-BADOL",
+        description: "Owner information",
+        category: "owner",
+        guide: "{p}"
+    },
+  onStart: async ({ api, message ,event }) => {
+        try {
+            const botName = "MIM-BOT-007";
+            const botPrefix = "/";
+            const authorName = "MOHAMMAD-BADOL";
+            const ownAge = "27";
+            const teamName = "MCS team";
+      const authorFB = "https://www.facebook.com/MBC.K1NG.007";
+          const authorInsta = "https://m.me/MBC.K1NG.007";
+  const link = "https://i.imgur.com/740LlU0.jpeg";
+        const now = moment().tz('Asia/Dhaka');
+      const date = now.format('MMMM Do YYYY');
+          const time = now.format('h:mm:ss A');
+            const uptime = process.uptime();
+      const seconds = Math.floor(uptime % 60);
+const minutes = Math.floor((uptime / 60) % 60);
+const hours = Math.floor((uptime / (60 * 60)) % 24);
+const days = Math.floor(uptime / (60 * 60 * 24));
+const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+            const buttons = {
+                inline_keyboard: [
+                    [
+        { text: "Facebook", url: authorFB },
+  { text: "Instagram", url: `https://t.me/${authorInsta}` }
+                    ]
+                ]
+            };
+
+            const caption = `
+• Bot & Owner Info
+╰‣ Bot Name: ${botName}
+╰‣ Bot Prefix: ${botPrefix}
+╰‣ Owner: ${authorName}
+╰‣ Age: ${ownAge}
+╰‣ Facebook: ${authorFB}
+╰‣ Messenger: ${authorInsta}
+╰‣ Date: ${date}
+╰‣ Time: ${time}
+╰‣ Team: ${teamName}
+╰‣ Uptime: ${uptimeString}`;
+
+            const hh = await api.sendPhoto(event.chat.id ,link, {caption: caption, reply_markup: buttons });
+            setTimeout(() => {
+             message.unsend(hh.message_id);
+            }, 40000);
+        } catch (error) {
+            console.error("Error reading config file:", error);
+        }
+    },
+    onChat: async function ({ event, message }) {
+        if (event.body?.toLowerCase() === "info" || event.body?.toLowerCase() === "owner") {
+            this.onStart({ message });
+        }
+    }
+};
